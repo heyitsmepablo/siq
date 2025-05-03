983bf2d1e3d0d613d71c7099a7770ef0cc671ba0
heyitsmepablo <pabloeduardossantos@gmail.com>
2025-05-03 01:31:02 -0300

    feat(api): documentação do swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Sistema Integrado de Qualidade')
+    .setDescription('API Rest para comunicação com banco de dados')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/doc', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
