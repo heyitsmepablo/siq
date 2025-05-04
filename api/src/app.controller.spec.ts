855e3def6564eb798915afa4368be31a5a1fd757
heyitsmepablo <pabloeduardossantos@gmail.com>
2025-05-03 21:44:30 -0300

    test(api): testes unitarios do app controller, auth service, users service

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,24 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserCreateDto } from './dtos/users.dto';
 import { prismaMock } from './__mock__/prisma-singleton';
+import { randomUUID } from 'crypto';
+import { LoginDto } from './dtos/auth.dto';
+import { AuthService } from './services/auth/auth.service';
+import { authServiceMock } from './__mock__/services/auth.service';
+import { UsersService } from './services/users/users.service';
+import { usersServiceMock } from './__mock__/services/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+        {
+          provide: UsersService,
+          useValue: usersServiceMock,
+        },
+      ],
     }).compile();
     appController = app.get<AppController>(AppController);
   });
 
   describe('register', () => {
-    it('deve resolver com mesagem de sucesso', async () => {
+    it('Deve resolver com mesagem de sucesso', async () => {
       const payload: UserCreateDto = { username: 'teste', password: 'teste' };
       prismaMock.users.create.mockResolvedValue({
-        id: 1,
+        id: randomUUID(),
+        accessToken: null,
+        lastLogin: null,
+        tokenExpiration: null,
         password: '',
         username: '',
         updatedAt: null,
@@ -27,6 +48,43 @@ describe('AppController', () => {
       await expect(appController.signUp(payload)).resolves.toEqual({
         message: 'success',
       });
+    });
+  });
+
+  describe('login', () => {
+    it('Deve resolver retornando payload de autenticado', async () => {
+      const requestPayload: LoginDto = { username: 'teste', password: 'teste' };
+      const expectedPayload = {
+        token: expect.any(String),
+        tokenType: expect.stringContaining('Bearer'),
+        expireIn: expect.any(Number),
+        expireAt: expect.any(Date), // é um Date (não string)
+        user: {
+          id: expect.any(String),
+          username: expect.any(String),
+          lastLogin: expect.toBeOneOf([expect.any(Date), null]),
+          createdAt: expect.any(Date), // timestamp em ms
+          updatedAt: expect.toBeOneOf([expect.any(Date), null]),
+        },
+      };
+      authServiceMock.signIn.mockResolvedValue({
+        token: 'string',
+        tokenType: 'Bearer',
+        expireIn: Math.floor(new Date(Date.now()).getTime() / 1000),
+        expireAt: new Date(Date.now()),
+        user: {
+          id: randomUUID(),
+          username: 'teste',
+          password: 'teste',
+          lastLogin: null,
+          createdAt: new Date(Date.now()),
+          updatedAt: new Date(Date.now()),
+        },
+      });
+
+      await expect(appController.login(requestPayload)).resolves.toMatchObject(
+        expectedPayload,
+      );
     });
   });
 });
---
+++ api/src/services/auth/auth.service.spec.ts
@@ -0,0 +1,96 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import 'jest-extended';
+import { JwtModule } from '@nestjs/jwt';
+import { randomUUID } from 'crypto';
+import { usersServiceMock } from '../../__mock__/services/users.service';
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: usersServiceMock,
+        },
+      ],
+      imports: [
+        JwtModule.register({
+          global: true,
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+    }).compile();
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('Deve está definido', () => {
+    expect(service).toBeDefined();
+  });
+  it('Deve resolver retornando payload com as informações do login', async () => {
+    const requestBody = { username: 'pablo', password: 'pablo' };
+    const expectedPayload = {
+      token: expect.any(String),
+      tokenType: expect.stringContaining('Bearer'),
+      expireIn: expect.any(Number),
+      expireAt: expect.any(Date),
+      user: {
+        id: expect.any(String),
+        username: expect.any(String),
+        lastLogin: expect.toBeOneOf([expect.any(Date), null]),
+        createdAt: expect.toBeOneOf([
+          expect.any(Date),
+          expect.any(String),
+          null,
+        ]), // timestamp em ms
+        updatedAt: expect.toBeOneOf([
+          expect.any(Date),
+          expect.any(String),
+          null,
+        ]),
+      },
+    };
+
+    usersServiceMock.findOne.mockResolvedValue({
+      id: randomUUID(),
+      username: 'pablo',
+      password: 'pablo',
+      lastLogin: null,
+      createdAt: new Date(Date.now()).toISOString(),
+      updatedAt: null,
+    });
+
+    usersServiceMock.updateOne.mockResolvedValue({ message: 'success' });
+
+    await expect(service.signIn(requestBody)).resolves.toMatchObject(
+      expectedPayload,
+    );
+  });
+  it('Deve rejeitar ao não encontrar usuario e jogar mensagem de erro', async () => {
+    const requestBody = { username: 'pablo', password: 'pablo' };
+
+    usersServiceMock.findOne.mockResolvedValue(null);
+
+    await expect(service.signIn(requestBody)).rejects.toThrow(
+      new UnauthorizedException('Usuario ou senha incorreto'),
+    );
+  });
+  it('Deve rejeitar caso a senha não corresponda e jogar mensagem de erro', async () => {
+    const requestBody = { username: 'pablo', password: 'pablo' };
+
+    usersServiceMock.findOne.mockResolvedValue({
+      username: 'pablo',
+      password: '1234',
+    });
+
+    await expect(service.signIn(requestBody)).rejects.toThrow(
+      new UnauthorizedException('Usuario ou senha incorreto'),
+    );
+  });
+});
